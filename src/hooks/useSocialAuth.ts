01cedc815b13f5949184eaabae408a3c83d69107
Jongkeun Choi <jongkeun.ch@gmail.com>
2021-05-11 22:31:11 +0900

    feat: 로그인 영속성 개선-로컬스토리지

--- src/hooks/useSocialAuth.ts
+++ src/hooks/useSocialAuth.ts
@@ -1,9 +1,51 @@
 import { useEffect } from "react";
 import { AuthState, ITwitter } from "../entity/AuthType";
 import { UpdateUserInfoAction } from "../entity/UserInfo";
+import { useLocalStorage } from "./useLocalStorage";
+
+function applyLogin(
+  authState: AuthState,
+  dispatch: React.Dispatch<UpdateUserInfoAction>
+) {
+  // 닉네임, 프로필사진 설정
+  const { authMethod, authValue } = authState;
+  if (authMethod) {
+    switch (authMethod) {
+      case "twitter":
+        const { displayName, id, photo, authorization } = authValue as ITwitter;
+        dispatch({
+          type: "UPDATE_USER_INFO",
+          payload: {
+            auth: {
+              authMethod: "twitter",
+              authValue: { displayName, id, photo, authorization },
+            },
+            userId: id,
+            nickname: displayName ?? "",
+            profile: photo ?? "https://via.placeholder.com/150",
+          },
+        });
+        break;
+      default:
+        break;
+    }
+  }
+}
 
 function useSocialAuth(dispatch: React.Dispatch<UpdateUserInfoAction>) {
-  // 소셜 인증 처리
+  // const [savedAuth, setSavedAuth] = useLocalStorage<AuthState>(
+  //   "roco-moe-comments-auth",
+  //   {
+  //     authMethod: null,
+  //     authValue: null,
+  //   }
+  // );
+
+  // 로컬스토리지에 인증 관련 정보가 있는 경우 이것을 사용함.
+  // if (savedAuth && savedAuth.authMethod && savedAuth.authValue) {
+  //   applyLogin(savedAuth, dispatch);
+  // }
+  // 소셜 로그인 수행 후 팝업이 메시지를 보냄
   useEffect(() => {
     function receiveMessage(event: MessageEvent<AuthState>) {
       // 허용 리스트
@@ -15,36 +57,13 @@ function useSocialAuth(dispatch: React.Dispatch<UpdateUserInfoAction>) {
       if (!allowedOrigin.includes(event.origin)) {
         return;
       }
-      // 닉네임, 프로필사진 설정
-      if (event.data.authMethod) {
-        const { authMethod, authValue } = event.data;
-        switch (authMethod) {
-          case "twitter":
-            const {
-              displayName,
-              id,
-              photo,
-              authorization,
-            } = authValue as ITwitter;
-            dispatch({
-              type: "UPDATE_USER_INFO",
-              payload: {
-                auth: {
-                  authMethod: "twitter",
-                  authValue: { displayName, id, photo, authorization },
-                },
-                userId: id,
-                nickname: displayName ?? "",
-                profile: photo ?? "https://via.placeholder.com/150",
-              },
-            });
-            (event.source as Window).close();
-            break;
-          default:
-            break;
-        }
-      }
+      (event.source as Window).close();
+      // 로그인 처리
+      applyLogin(event.data, dispatch);
+      // 로컬스토리지에 저장
+      // setSavedAuth(event.data);
     }
+
     window.addEventListener("message", receiveMessage, false);
     return () => {
       window.removeEventListener("message", receiveMessage);
